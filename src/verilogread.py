ee76cddcb75dd38f5bb021be16e474b362b086df
BOBO <boyuanspace@gmail.com>
2024-05-16 21:30:11 +0800

    change read as a function

--- src/verilogread.py
+++ src/verilogread.py
@@ -1,85 +1,87 @@
-filename = "design1.v"
 
-with open("./netlists/"+filename, "r") as file:
-  contents = file.read()
+def veryread(filename):
 
-lines = contents.split(";")
+    with open(filename, "r") as file:
+        contents = file.read()
 
-# inputs
-inputs_data = lines[1].split("n")
-input = []
-for i in inputs_data:
-    if (i.startswith("0") or 
-        i.startswith("1") or 
-        i.startswith("2") or 
-        i.startswith("3") or 
-        i.startswith("4") or 
-        i.startswith("5") or 
-        i.startswith("6") or
-        i.startswith("7") or 
-        i.startswith("8") or 
-        i.startswith("9") ):
-        input.append(int(i.split(" ")[0]))
-print(f"Input is {input}",)
+    lines = contents.split(";")
 
-# outputs
-outputs_data = lines[2].split("n")
-output = []
-for i in outputs_data:
-    if (i.startswith("0") or 
-        i.startswith("1") or 
-        i.startswith("2") or 
-        i.startswith("3") or 
-        i.startswith("4") or 
-        i.startswith("5") or 
-        i.startswith("6") or
-        i.startswith("7") or 
-        i.startswith("8") or 
-        i.startswith("9") ):
-        output.append(int(i.split(" ")[0]))
-print(f"Output is {output}",)
+    # inputs
+    inputs_data = lines[1].split("n")
+    input = []
+    for i in inputs_data:
+        if (i.startswith("0") or 
+            i.startswith("1") or 
+            i.startswith("2") or 
+            i.startswith("3") or 
+            i.startswith("4") or 
+            i.startswith("5") or 
+            i.startswith("6") or
+            i.startswith("7") or 
+            i.startswith("8") or 
+            i.startswith("9") ):
+            input.append(int(i.split(" ")[0]))
+    print(f"Input is {input}",)
 
-# wire
-wire_data = lines[3].split("n")
-wire = []
-for i in wire_data:
-    if (i.startswith("0") or 
-        i.startswith("1") or 
-        i.startswith("2") or 
-        i.startswith("3") or 
-        i.startswith("4") or 
-        i.startswith("5") or 
-        i.startswith("6") or
-        i.startswith("7") or 
-        i.startswith("8") or 
-        i.startswith("9") ):
-        wire.append(int(i.split(" ")[0]))
-print(f"Wire is {wire}")
+    # outputs
+    outputs_data = lines[2].split("n")
+    output = []
+    for i in outputs_data:
+        if (i.startswith("0") or 
+            i.startswith("1") or 
+            i.startswith("2") or 
+            i.startswith("3") or 
+            i.startswith("4") or 
+            i.startswith("5") or 
+            i.startswith("6") or
+            i.startswith("7") or 
+            i.startswith("8") or 
+            i.startswith("9") ):
+            output.append(int(i.split(" ")[0]))
+    print(f"Output is {output}",)
 
-#gates
-gate = []
+    # wire
+    wire_data = lines[3].split("n")
+    wire = []
+    for i in wire_data:
+        if (i.startswith("0") or 
+            i.startswith("1") or 
+            i.startswith("2") or 
+            i.startswith("3") or 
+            i.startswith("4") or 
+            i.startswith("5") or 
+            i.startswith("6") or
+            i.startswith("7") or 
+            i.startswith("8") or 
+            i.startswith("9") ):
+            wire.append(int(i.split(" ")[0]))
+    print(f"Wire is {wire}")
 
-loc = 4
-while True:
-    if(lines[loc].startswith("\nendmodule")):
-        break
-    temp = []
-    for i in lines[loc].split(" "):
-        if (
-        i.startswith("a") or 
-        i.startswith("o") or
-        i.startswith("g") or 
-        i.startswith("n") or 
-        i.startswith("b") or 
-        i.startswith("x")):
-            temp.append(i)
+    #gates
+    gate = []
 
-    if (len(temp) == 5):
-        gate.append(temp)
-    if (len(temp) == 4):
-        temp = temp[0:3] + temp[2:3] + temp[3:]
-        gate.append(temp)
+    loc = 4
+    while True:
+        if(lines[loc].startswith("\nendmodule")):
+            break
+        temp = []
+        for i in lines[loc].split(" "):
+            if (
+            i.startswith("a") or 
+            i.startswith("o") or
+            i.startswith("g") or 
+            i.startswith("n") or 
+            i.startswith("b") or 
+            i.startswith("x")):
+                temp.append(i)
 
-    loc += 1
+        if (len(temp) == 5):
+            gate.append(temp)
+        if (len(temp) == 4):
+            temp = temp[0:3] + temp[2:3] + temp[3:]
+            gate.append(temp)
 
-print(f"gate is {gate}")
+        loc += 1
+
+    print(f"gate is {gate}")
+    return input, output, wire, gate
