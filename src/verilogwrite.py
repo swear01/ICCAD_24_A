0720b57f99ad93fbb3483e89dacd49c6dd8c26e9
BOBO <boyuanspace@gmail.com>
2024-05-16 21:42:35 +0800

    writeverilog

--- src/verilogwrite.py
+++ src/verilogwrite.py
@@ -1,5 +1,9 @@
-filename = "output.v"
+def verywrite(filename, input, output, gate):
+    filename = "output.v"
 
-with open("./netlists/"+filename, "w") as file:
-   file.write('world')
-file.close()
+    with open("./netlists/"+filename, "w") as file:
+        file.write('module')
+        file.write('input')
+        file.write('output')
+        file.write('endmodule')
+    file.close()
