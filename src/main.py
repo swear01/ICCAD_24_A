4fb54b22d9982f52b31af3c6859d16fb06a2e186
swear01 <stanley.yellow1@gmail.com>
2024-06-14 10:51:56 +0800

    renaming

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import abc_cmd
 import map_annealing
-import verilog_read
-import pick_greedy_threegate
+from verilog_read import abc_read_verilog, read_verilog
+from verilog_write import write_verilog
 import pick_singlegate
 
 # Logic synthesis with abc tool and simulated annealing
@@ -13,6 +13,7 @@ filename = "design1.v"
 assert filename.endswith(".v")
 cmd = abc_cmd.get_random_cmd(folder, out_folder, gate_lib_path, filename)
 abc_cmd.abc_exec(abc_path, cmd)
+write_verilog(out_folder + filename[:-2] + "_abc.v" ,*abc_read_verilog(out_folder + filename[:-2] + "_abc.v"))
 abc_cmd.abc_print(abc_path, out_folder, filename[:-2] + "_abc.v")
 
 
@@ -23,8 +24,8 @@ library_path = "data/lib/lib1.json"
 output_path = "output/output.v"
 cost_estimator_path = "data/cost_estimators/cost_estimator_2"
 
-# dictionary = mappingannealing.initial_mapping_determine(netlist_path, cost_estimator_path, library_path)
-# mappingannealing.mapping_annealing(netlist_path, cost_estimator_path, library_path, output_path, dictionary)
+dictionary = map_annealing.initial_mapping_determine(netlist_path, cost_estimator_path, library_path)
+map_annealing.map_annealing(netlist_path, cost_estimator_path, library_path, output_path, dictionary)
 
-verilog_read.abc_read_verilog("tmp/design1_abc.v")
-pick_singlegate.get_cost(cost_estimator_path, netlist_path, library_path, output_path)
+read_verilog("tmp/design1_abc.v")
+pick_singlegate.get_cost(cost_estimator_path, "tmp/design1_abc.v", library_path, output_path)
