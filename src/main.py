5430052b9fe2b97fb6b19bb0597aec172c91449d
BOBO <boyuanspace@gmail.com>
2024-06-14 11:05:17 +0800

    main not done yet

--- src/main.py
+++ src/main.py
@@ -1,9 +1,31 @@
 import abc_cmd
 import map_annealing
+<<<<<<< HEAD
 from verilog_read import abc_read_verilog, read_verilog
 from verilog_write import write_verilog
 import pick_singlegate
+=======
+import verilog_read
+>>>>>>> cf010a8 (main not done yet)
 
+# greedy
+
+#
+for i in range(1,7):
+    for j in range(1,9):
+        print(i,j)
+        s1 = "data/netlists/design" + str(i) + ".v"
+        s2 = "data/cost_estimators/cost_estimator_" + str(j)
+        s3 = "data/lib/lib1.json"
+        s4 = "output/output.v"
+        # verilog_file_path = abc_annealing(s1, s2, s3, s4)
+        # mapping_annealing(verilog_file_path, s2, s3, s4)
+
+        dictionary = map_annealing.initial_mapping_determine(s1, s2, s3)
+        verilog_file_path = map_annealing.abc_annealing(s1, s2, s3, s4, dictionary)
+        map_annealing.mapping_annealing(verilog_file_path, s2, s3, s4, dictionary)
+
+'''
 # Logic synthesis with abc tool and simulated annealing
 abc_path = "./src/abc"
 gate_lib_path = "./data/lib/lib1.genlib"
@@ -24,3 +46,12 @@ library_path = "data/lib/lib1.json"
 output_path = "output/output.v"
 cost_estimator_path = "data/cost_estimators/cost_estimator_2"
 
+<<<<<<< HEAD
+=======
+# dictionary = mappingannealing.initial_mapping_determine(netlist_path, cost_estimator_path, library_path)
+# mappingannealing.mapping_annealing(netlist_path, cost_estimator_path, library_path, output_path, dictionary)
+
+verilog_read.abc_read_verilog("tmp/design1_abc.v")
+pick_singlegate.get_cost(cost_estimator_path, netlist_path, library_path, output_path)
+'''
+>>>>>>> cf010a8 (main not done yet)
