839ef3af58ee773c6e058abd54fb4c4348bc1a69
swear01 <stanley.yellow1@gmail.com>
2024-06-14 11:12:37 +0800

    clean map_annealing

--- src/pick_singlegate.py
+++ src/pick_singlegate.py
@@ -13,29 +13,32 @@ modulename = "module top_1598227639_809568180_776209382_1234615"
 cost_estimator_path = "data/cost_estimators/cost_estimator_1"
 library_path = "data/lib/lib1.json"
 tmp_output_path = "data/gate/output.txt"
-
-
-best_gate =  {}
-choices = number_of_choices(json.load(open(library_path)))
-for gate in gate_list:
-    costs = []
-    if is_single_gate(gate):
-        inputs, outputs = ["n1"], ["n3"]
-        gates = [[f"{gate}_{i}", "g0", "n1", "n3"] for i in range(1, choices[gate] + 1)]
-    else:
-        inputs, outputs = ["n1" , "n2"], ["n3"]
-        gates = [[f"{gate}_{i}", "g0", "n1", "n2", "n3"] for i in range(1, choices[gate] + 1)]
-                    
-    for gate_i in gates:
-        verilog_write.write_verilog(filename, modulename, inputs, outputs, [], [gate_i])
-        cost = get_cost(cost_estimator_path, filename, library_path, tmp_output_path)
-        costs.append(cost)
-    best_number = costs.index(min(costs)) + 1
-    best_gate[gate] = best_number
-
 output_file_path = './data/gate/best_single_gate.json'
-with open(output_file_path, 'w') as json_file:
-    json.dump(best_gate, json_file)
+
+def find_initial_mapping(modulename, cost_estimator_path, library_path,):
+    best_gate =  {}
+    choices = number_of_choices(json.load(open(library_path)))
+    for gate in gate_list:
+        costs = []
+        if is_single_gate(gate):
+            inputs, outputs = ["n1"], ["n3"]
+            gates = [[f"{gate}_{i}", "g0", "n1", "n3"] for i in range(1, choices[gate] + 1)]
+        else:
+            inputs, outputs = ["n1" , "n2"], ["n3"]
+            gates = [[f"{gate}_{i}", "g0", "n1", "n2", "n3"] for i in range(1, choices[gate] + 1)]
+                        
+        for gate_i in gates:
+            verilog_write.write_verilog(filename, modulename, inputs, outputs, [], [gate_i])
+            cost = get_cost(cost_estimator_path, filename, library_path, tmp_output_path)
+            costs.append(cost)
+        best_number = costs.index(min(costs)) + 1
+        best_gate[gate] = best_number
+
+
+    with open(output_file_path, 'w') as json_file:
+        json.dump(best_gate, json_file)
+        
+    os.remove(filename)
+    os.remove(tmp_output_path)
     
-os.remove(filename)
-os.remove(tmp_output_path)
+    return best_gate
